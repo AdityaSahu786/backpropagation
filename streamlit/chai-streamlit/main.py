9d76b6605e0c1797a08032958035c2dab1594b2a
AdityaSahu <sahuadi786183@gmail.com>
2026-06-16 14:07:14 +0530

    feat: initialised streamlit

---
+++ streamlit/chai-streamlit/main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Hello Chai App")
+st.subheader("Brewed with streamlit")
+st.text("Welcome to your first Interactive app")
+st.write("Choose your fav. variety of chai")
+
+chai = st.selectbox("Your fav chai: ", ["Masala Chai", "Lemon Tea", "Adrak Chai", "Kesar chai"])
