34b9ec8ebe06dfc4b5f6ab2a86f56c0f512ae96c
AdityaSahu <sahuadi786183@gmail.com>
2026-06-16 14:13:51 +0530

    feat: assignment 1

--- streamlit/chai-streamlit/main.py
+++ streamlit/chai-streamlit/main.py
@@ -1,8 +1,21 @@
 import streamlit as st
 
+# st.title("Hello Chai App")
+# st.subheader("Brewed with streamlit")
+# st.text("Welcome to your first Interactive app")
+# st.write("Choose your fav. variety of chai")
+
+# chai = st.selectbox("Your fav chai: ", ["Masala Chai", "Lemon Tea", "Adrak Chai", "Kesar chai"])
+# st.write(f"You choose {chai}. Excellent choice")
+
+# st.success("Your chai has been brewed")
+
 st.title("Hello Chai App")
 st.subheader("Brewed with streamlit")
 st.text("Welcome to your first Interactive app")
-st.write("Choose your fav. variety of chai")
+st.write("Choose your fav. variety of coding language")
 
-chai = st.selectbox("Your fav chai: ", ["Masala Chai", "Lemon Tea", "Adrak Chai", "Kesar chai"])
+chai = st.selectbox("Your fav chai: ", ["C", "C++", "JAVA", "PYTHON", "Rust"])
+st.write(f"You choose {chai}. Excellent choice")
+
+st.success("Your chai has been brewed")
